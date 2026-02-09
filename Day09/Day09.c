8e8e94432d7994f73d2d4c833cf9eb5f182cc599
varnika-agarwal <varnikaa.1802@gmail.com>
2026-02-09 21:45:00 +0530

    Add files via upload

---
+++ Day09/Day09.c
@@ -0,0 +1,16 @@
+//Problem: A secret system stores code names in forward order. To display them in mirror format, you must transform the given code name so that its characters appear in the opposite order.
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, length = 0;
+    scanf("%s", str);
+    while (str[length] != '\0') {
+        length++;
+    }
+    for (i = length - 1; i >= 0; i--) {
+        printf("%c", str[i]);
+    }
+
+    return 0;
+}
